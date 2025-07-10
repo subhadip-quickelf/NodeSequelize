0117b5a5660e20615fe59abdbb25dbe8c272283b
Subhadip Dutta <subhadip.dutta@magicworkshop.ai>
2025-07-10 21:47:08 +0530

    Deploys application as a serverless function
    Configures the application to be deployed as a serverless function on Netlify.
    
    This involves:
    - Adding `serverless-http` to wrap the Express app.
    - Creating a Netlify function handler.
    - Updating the build and serve scripts.
    - Adding a `netlify.toml` file for configuration.
    - Modifying the `tsconfig.json` to output to the netlify functions directory.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,6 @@
-import "dotenv/config";
-import express, { Request, Response } from "express";
-import User from "./models/user";
-require("./models");
+import app from "./app";
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello, TypeScript Express!");
-});
-
-app.post("/user", async (req: Request, res: Response) => {
-	try {
-		const user = await User.build({
-			name: req.body.name,
-			email: req.body.email,
-		});
-		await user.save();
-		res.status(201).send(user.toJSON());
-	} catch (err) {
-		res.status(500).send(err);
-	}
-});
 
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);

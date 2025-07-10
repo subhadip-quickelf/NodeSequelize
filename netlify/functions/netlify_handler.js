ad8e42bfe56cfe5a4c43c72a6e850437f1fb3ca9
Subhadip Dutta <subhadip.dutta@magicworkshop.ai>
2025-07-10 23:07:18 +0530

    Removes unnecessary database connection attempt
    Removes an unnecessary attempt to create a database connection, which was causing issues. This change simplifies the setup and avoids potential errors.

--- netlify/functions/netlify_handler.js
+++ netlify/functions/netlify_handler.js
@@ -4,11 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
-const promise_1 = __importDefault(require("mysql2/promise"));
+require("mysql2");
 const serverless_http_1 = __importDefault(require("serverless-http"));
 const app_1 = __importDefault(require("./app"));
-try {
-    promise_1.default.createConnection({});
-}
-catch (_) { }
 exports.handler = (0, serverless_http_1.default)(app_1.default);
